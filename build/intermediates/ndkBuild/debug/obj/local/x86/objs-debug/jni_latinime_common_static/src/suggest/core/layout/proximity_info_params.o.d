79e47dbd7d5392bcd25dced8cbb02878ac71c690
tianhongliang <arvin@wokoworks.com>
2018-12-20 13:50:20 +0800

    删除多余文件

--- build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/jni_latinime_common_static/src/suggest/core/layout/proximity_info_params.o.d
+++
@@ -1,8 +0,0 @@
-/Users/pundix42/Desktop/inputmethods/LatinIME/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/jni_latinime_common_static/src/suggest/core/layout/proximity_info_params.o: \
-  /Users/pundix42/Desktop/inputmethods/LatinIME/native/jni/src/suggest/core/layout/proximity_info_params.cpp \
-  /Users/pundix42/Desktop/inputmethods/LatinIME/native/jni/src/defines.h \
-  /Users/pundix42/Desktop/inputmethods/LatinIME/native/jni/src/suggest/core/layout/proximity_info_params.h
-
-/Users/pundix42/Desktop/inputmethods/LatinIME/native/jni/src/defines.h:
-
-/Users/pundix42/Desktop/inputmethods/LatinIME/native/jni/src/suggest/core/layout/proximity_info_params.h:
