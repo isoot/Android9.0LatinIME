28bd3f17e0da3206e78d3f4c9b460355153ae3d5
tianhongliang <arvin@wokoworks.com>
2018-12-21 14:30:41 +0800

    删除多余文件

--- build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/jni_latinime_common_static/src/dictionary/structure/backward/v402/ver4_dict_constants.o.d
+++
@@ -1,8 +0,0 @@
-/Users/pundix42/Desktop/Android9.0LatinIME/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/jni_latinime_common_static/src/dictionary/structure/backward/v402/ver4_dict_constants.o: \
-  /Users/pundix42/Desktop/Android9.0LatinIME/native/jni/src/dictionary/structure/backward/v402/ver4_dict_constants.cpp \
-  /Users/pundix42/Desktop/Android9.0LatinIME/native/jni/src/dictionary/structure/backward/v402/ver4_dict_constants.h \
-  /Users/pundix42/Desktop/Android9.0LatinIME/native/jni/src/defines.h
-
-/Users/pundix42/Desktop/Android9.0LatinIME/native/jni/src/dictionary/structure/backward/v402/ver4_dict_constants.h:
-
-/Users/pundix42/Desktop/Android9.0LatinIME/native/jni/src/defines.h:
